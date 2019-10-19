f722508c2805afb35eaa4e2b829970a411c355fc
Tianyang Xie <tianyang.xie.xty@gmail.com>
2019-10-19 16:37:42 -0500

    Update FSARMAfit.R

--- R/FSARMAfit.R
+++ R/FSARMAfit.R
@@ -1,6 +1,6 @@
 # freq fitting
-FSARMA.fit =function(X,U,p,q,S1,S2,W=NULL,k=2){
-  
+FSARMA.fit =function(X,U,p,q,S1,S2,W=NULL,crit='AIC'){
+
   if (is.null(W)){
     W = t(as.matrix(rep(0,nrow(X))))
   }
@@ -33,23 +33,28 @@ FSARMA.fit =function(X,U,p,q,S1,S2,W=NULL,k=2){
     if (is.null(d)){gamma=NULL
     }else{gamma = beta[(p+q+r1+r2+1):(p+q+r1+r2+d)]}
     sigma = sqrt(Rfit(beta)/(nrow(X)-U))
-    if (k==-1){
-      X.temp = X[1:(nrow(X)-max(c(S1.int,S2.int))),1,drop=F]
-      m.temp = FSARMA.fit(X.temp,U,p,q,S1,S2,W,k=2)
-      prediction = FSARMA.pred(m.temp,X.temp,W,pred_t=max(c(S1.int,S2.int)))
-      aic = sum(abs(prediction[(nrow(X)-max(c(S1.int,S2.int))+1):(nrow(X))]-
-                      X[(nrow(X)-max(c(S1.int,S2.int))+1):(nrow(X))]))
-    }else{
-      mdl = likeli(X,U,
-                   as.double(phi),p,
-                   as.double(psi),q,
-                   as.double(tau1),S1.int,r1,
-                   as.double(tau2),S2.int,r2,
-                   as.double(gamma), W, as.double(sigma))
-      aic =  - 2*mdl + k*(p+q+r1+r2+d+1)
+
+    mdl = likeli(X,U,
+                 as.double(phi),p,
+                 as.double(psi),q,
+                 as.double(tau1),S1.int,r1,
+                 as.double(tau2),S2.int,r2,
+                 as.double(gamma), W, as.double(sigma))
+    dim = (p+q+length(S1.int)+length(S2.int)+ifelse(is.null(W),0,d))
+
+    if (crit=='AIC'){
+      ic = - 2*mdl + 2*dim
     }
 
-    ult = list(U,p,phi,q,psi,r1,S1,tau1,r2,S2,tau2,gamma,sigma,aic)
+    if (crit=='BIC'){
+      ic = - 2*mdl + log(nrow(X))*dim
+    }
+
+    if (crit=='BC'){
+      ic = -2*mdl + (nrow(X)^(2/3))*(sum(1/c(1:dim)))
+    }
+
+    ult = list(U,p,phi,q,psi,r1,S1,tau1,r2,S2,tau2,gamma,sigma,ic)
   },error=function(cond){
     if (p>0){phi=rep(0,p)
     }else{phi=NULL}
@@ -62,8 +67,8 @@ FSARMA.fit =function(X,U,p,q,S1,S2,W=NULL,k=2){
     if (!is.null(d)){gamma=rep(0,d)
     }else{gamma=NULL}
     sigma = 1
-    aic = Inf
-    ult = list(U,p,phi,q,psi,r1,S1,tau1,r2,S2,tau2,gamma,sigma,aic)
+    ic = Inf
+    ult = list(U,p,phi,q,psi,r1,S1,tau1,r2,S2,tau2,gamma,sigma,ic)
     # message(paste(c("Failure of fitting:    ",
     #                 'p=',p,' ','q=',q,' ',
     #                 'S1=c(',paste(S1,collapse=','),') ',
@@ -72,7 +77,7 @@ FSARMA.fit =function(X,U,p,q,S1,S2,W=NULL,k=2){
   })
 
   names(ult) = c('U','p','phi','q','psi','r1','S1','tau1','r2','S2','tau2','gamma',
-                 'sigma','aic')
+                 'sigma','ic')
   return(ult)
 
 }
