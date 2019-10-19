d0966bcfb3af3e58626e20c33bfb79300e8adce9
Tianyang Xie <tianyang.xie.xty@gmail.com>
2019-10-19 16:40:55 -0500

    Update parallelfit.R

--- R/parallelfit.R
+++ R/parallelfit.R
@@ -1,5 +1,5 @@
-parallel.fit = function(X,W.list,para_panel,U,k,multicore=F){
-  
+parallel.fit = function(X,W.list,para_panel,U,crit,multicore=F){
+
   if (!multicore){
     # single core
     for (i in 1:nrow(para_panel)){
@@ -11,16 +11,16 @@ parallel.fit = function(X,W.list,para_panel,U,k,multicore=F){
       if (is.na(temp_row[1,4])){S2=NULL
       }else{S2 = as.integer(strsplit(temp_row[1,4],' ')[[1]])}
       W.temp = rbindlist(W.list,temp_row[1,5])
-      
-      m = FSARMA.fit(X,U,p,q,S1,S2,W.temp,k)
-      para_panel[i,6] = m$aic
+
+      m = FSARMA.fit(X,U,p,q,S1,S2,W.temp,crit)
+      para_panel[i,6] = m$ic
     }
-    
+
   }else{
     # multiple cores
     ncores = min(detectCores(),nrow(para_panel))
     cl = makePSOCKcluster(ncores)
-    
+
     l = floor(nrow(para_panel)/ncores)
     assign_loc = rep(l,ncores)
     if (nrow(para_panel)%%ncores!=0){
@@ -35,7 +35,7 @@ parallel.fit = function(X,W.list,para_panel,U,k,multicore=F){
       assign_list[[i]] = c(a:assign_loc_cums[i])
       a = 1 + assign_loc_cums[i]
     }
-    
+
     singleFit = function(core_ind,assign_list,para_panel){
       index_set = assign_list[[core_ind]]
       aic_set = rep(0,length(index_set))
@@ -49,23 +49,23 @@ parallel.fit = function(X,W.list,para_panel,U,k,multicore=F){
         if (is.na(temp_row[1,4])){S2=NULL
         }else{S2 = as.integer(strsplit(temp_row[1,4],' ')[[1]])}
         W.temp = rbindlist(W.list,temp_row[1,5])
-        
-        m = FSARMA.fit(X,U,p,q,S1,S2,W.temp,k)
-        aic = m$aic
-        aic_set[i] = aic
+
+        m = FSARMA.fit(X,U,p,q,S1,S2,W.temp,crit)
+        ic = m$ic
+        ic_set[i] = ic
       }
-      return(aic_set)
+      return(ic_set)
     }
-    
+
     clusterExport(cl, list("FSARMA.fit","FSARMA.pred", "rbindlist","singleFit", "assign_list"
-                           ,"para_panel","W.list","X","U","k")
+                           ,"para_panel","W.list","X","U","crit")
                   , envir=environment())
     pout = parLapply(cl, c(1:ncores), singleFit, assign_list=assign_list
                      ,para_panel=para_panel)
-    aic_col = unlist(pout)
-    para_panel[,6] = aic_col
+    ic_col = unlist(pout)
+    para_panel[,6] = ic_col
     stopCluster(cl)
   }
-  
+
   return(para_panel)
 }
