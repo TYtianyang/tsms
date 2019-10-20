dbb632ead5b2c3b40d159b1878dbf6e13accf93a
Tianyang Xie <tianyang.xie.xty@gmail.com>
2019-10-20 10:58:57 -0500

    Update HSARMAauto.R

--- R/HSARMAauto.R
+++ R/HSARMAauto.R
@@ -2,15 +2,14 @@ HSARMA.auto = function(X,W=NULL,U,p_range,q_range,r=1,S=NULL,
                        blur.out=c(2,2),sar=T,sma=T,
                        sfourier=F,order=1,
                        sfactor=F,pred_t=0,
-                       k=2,level=0.05,multicore=T){
+                       crit='BC',level=0.05,multicore=T){
 
   # step 1: prepare
-  k0 = k
   X = as.matrix(X)
 
   # step 2: suggest seasonality
   if (is.null(S)){
-    S = suggest(X,level)
+    S = suggest(X,r+2,level)
   }
   S = unique(S[(S>(max(p_range,q_range)+blur.out[2]))&(S<(U-blur.out[1]))])
 
@@ -60,6 +59,7 @@ HSARMA.auto = function(X,W=NULL,U,p_range,q_range,r=1,S=NULL,
         }
       }
       if (sfactor){
+        S = round(S)
         start = as.numeric(sfourier)*order*2
         base.vec = rep(0,nrow(X)+pred_t)
         base.vec[1+S*(c(1:floor((nrow(X)+pred_t)/S))-1)] = 1
@@ -73,8 +73,8 @@ HSARMA.auto = function(X,W=NULL,U,p_range,q_range,r=1,S=NULL,
 
   # step 5: construct para_panel
   S_n = ncol(combn(c(1:length(S.pick)),r.nomial))
-  para_panel = data.frame(matrix(nrow=length(p_range)*length(q_range)*S_n,ncol=6))
-  colnames(para_panel) = c('p','q','S1','S2','W','aic')
+  para_panel = data.frame(matrix(nrow=length(p_range)*length(q_range)*S_n,ncol=7))
+  colnames(para_panel) = c('p','q','S1','S2','W','ic','dim')
 
   comb = combn(c(1:length(S.pick)),r.nomial)
   S_vec = c()
@@ -83,7 +83,6 @@ HSARMA.auto = function(X,W=NULL,U,p_range,q_range,r=1,S=NULL,
     S_vec[i] = paste(unlist(S.pick[as.vector(comb[,i])]),collapse=' ')
     W_vec[i] = paste(c(paste('sea',as.vector(comb[,i]),sep=''),'exo'),collapse=' ')
   }
-
   para_panel[,5] = rep(W_vec,length(p_range)*length(q_range))
   if (sar){
     para_panel[,3] = rep(S_vec,length(p_range)*length(q_range))
@@ -99,8 +98,23 @@ HSARMA.auto = function(X,W=NULL,U,p_range,q_range,r=1,S=NULL,
   para_panel[,2] = rep(rep(q_range,rep(S_n,length(q_range))),length(p_range))
 
   # step 6: fit and select optimal
-  para_panel = parallel.fit(X,W.list,para_panel,U,k0,multicore)
-  best.row = para_panel[which.min(para_panel[,6]),1:5]
+  para_panel = parallel.fit(X,W.list,para_panel,U,'AIC',multicore)
+
+  if (crit=='BC'){
+    dim_up = para_panel[which.min(para_panel[,6]),'dim']
+    for (i in 1:nrow(para_panel)){
+      dim = para_panel[i,'dim']
+      para_panel[i,'ic'] = para_panel[i,'ic'] - 2*dim + (nrow(X)^(2/3))*(sum(1/c(1:dim)))
+    }
+    temp_para_panel = para_panel[para_panel$dim<=dim_up,]
+    best.row = temp_para_panel[which.min(temp_para_panel[,6]),1:5]
+  }else{
+    if (crit=='BIC'){
+      para_panel$ic = para_panel$ic - 2*para_panel$dim + log(nrow(X))*para_panel$dim
+    }
+    best.row = para_panel[which.min(para_panel[,6]),1:5]
+  }
+
   p = best.row[1,1]
   q = best.row[1,2]
   if (is.na(best.row[1,3])){S1=NULL
@@ -108,8 +122,8 @@ HSARMA.auto = function(X,W=NULL,U,p_range,q_range,r=1,S=NULL,
   if (is.na(best.row[1,4])){S2=NULL
   }else{S2 = as.integer(strsplit(best.row[1,4],' ')[[1]])}
   W.temp = rbindlist(W.list,best.row[1,5])
-  best.m = FSARMA.fit(X,U,p,q,S1,S2,W.temp,k0)
+  best.m = FSARMA.fit(X,U,p,q,S1,S2,W.temp,crit)
   prediction = FSARMA.pred(best.m,X,W.temp,pred_t)
 
-  output = list(obj=best.m,aic_panel=para_panel,prediction=prediction)
+  output = list(obj=best.m,ic_panel=para_panel,prediction=prediction)
 }
